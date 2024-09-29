02cd184049d1e4a8fb95fec048cf30e75465f75c
Sara <sara@Saras-MBP-2.station>
2024-09-29 16:50:10 +0200

    Check on homepage icons

--- pantarei/Homepage.py
+++ pantarei/Homepage.py
@@ -4,14 +4,16 @@ import pandas as pd
 import streamlit as st
 
 dirfolder = os.getcwd()
-dirfolder = os.path.dirname(dirfolder)
-st.write(dirfolder)
 
+print  (dirfolder)
+
+st.write(dirfolder)
 
 st.image(os.path.join(dirfolder, "MSCA_logo.png"))
 
 st.title("PANTA REI: Perspectives on mitigation and adaptation of climate change")
 
 st.write("Welcome! We will see how mitigation and adaptation are in the modelling.")
+
 
 
